4537c88e3b359d03aa8ae4b4082eba98fd7f9904
Ian <yt2015.97@gmail.com>
2018-12-25 15:53:32 +0800

    ……

--- algorithm/LinkedList.js
+++ algorithm/LinkedList.js
@@ -14,7 +14,7 @@ class LinkedList {
         const node = new Node(element)
 
         if (!this.head) {
-            this.head = null
+            this.head = node
         } else {
             let current = this.head
 
@@ -29,16 +29,16 @@ class LinkedList {
         if (position < 0 || position >= this.length) {
             return null
         }
-        
+
         let current = this.head
-        if(position === 0){
+        if (position === 0) {
             this.head = current.next
             this.length--
             return current.element
         }
-        
+
         let previous = this.head
-        for(let index=0;index<position;index++){
+        for (let index = 0; index < position; index++) {
             previous = current
             current = current.next
         }
@@ -47,14 +47,37 @@ class LinkedList {
         this.length--
         return current.element
     }
-    insert(position, element){
+    remove(element) {
+        let current = this.head
+        if (this.head.element === element) {
+            this.head = current.next
+            this.length--
+            return current.element
+        }
+
+        let previous = this.head
+        while (current) {
+            if (current.element === element) {
+                previous.next = current.next
+                this.length--
+
+                return current.element
+            }
+
+            previous = current
+            current = current.next
+        }
+
+        return null
+    }
+    insert(position, element) {
         if (position < 0 || position >= this.length) {
             return false
         }
 
         const node = new Node(element)
-        let current = this,head
-        if(position === 0){
+        let current = this.head
+        if (position === 0) {
             node.next = current
             this.head = node
             this.length++
@@ -62,7 +85,7 @@ class LinkedList {
         }
 
         let previous = this.head
-        for(let index=0;index<position;index++){
+        for (let index = 0; index < position; index++) {
             previous = current
             current = current.next
         }
@@ -73,6 +96,55 @@ class LinkedList {
     }
     toString() {
         let string = ''
-        
+        let current = this.head
+
+        while (current) {
+            string = string + current.element + ' '
+            current = current.next
+        }
+
+        return string
+    }
+    indexOf(element) {
+        let current = this.head
+        let position = 0
+
+        while (current) {
+            if (current.element === element) {
+                return position
+            }
+
+            current = current.next
+            position++
+        }
+
+        return -1
+    }
+    isEmpty() {
+        return this.length === 0
+    }
+    size() {
+        return this.length
+    }
+    getHead() {
+        return this.head.element
+    }
+    print() {
+        console.log(
+            this.toString()
+                .trim()
+                .replace(/ /g, ' ==> ')
+        )
     }
 }
+
+const linkedList = new LinkedList()
+linkedList.append(0)
+linkedList.append(2)
+linkedList.append(3)
+linkedList.insert(1, 1)
+linkedList.insert(1, 1)
+linkedList.remove(3)
+linkedList.removeAt(2)
+
+linkedList.print()
